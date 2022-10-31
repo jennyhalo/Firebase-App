ea1c0530303e6f1a3ad6671e3ef6db92130c84d4
jennyhalo <jenny.halonen@student.laurea.fi>
2022-10-31 16:41:41 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'devdactic-tut-8a804',
+    appId: '1:116545914622:web:893690ce2ad03e39c847af',
+    storageBucket: 'devdactic-tut-8a804.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCczc93kyufWXYNAUsdK8Rm911i5OC9qV0',
+    authDomain: 'devdactic-tut-8a804.firebaseapp.com',
+    messagingSenderId: '116545914622',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'devdactic-tut-8a804',
+    appId: '1:116545914622:web:893690ce2ad03e39c847af',
+    storageBucket: 'devdactic-tut-8a804.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCczc93kyufWXYNAUsdK8Rm911i5OC9qV0',
+    authDomain: 'devdactic-tut-8a804.firebaseapp.com',
+    messagingSenderId: '116545914622',
+  },
   production: false
 };
 
